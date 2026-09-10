08acb52e759afe8816d0333a6b3e915dfe2164b2
himalii1206-coder <himalii1206@gmail.com>
2026-09-10 15:38:16 +0530

    fix production cors

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ const app: Application = express();
 // Middlewares
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: 'https://inquisitive-duckanoo-52fa3d.netlify.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Company-Id'],
